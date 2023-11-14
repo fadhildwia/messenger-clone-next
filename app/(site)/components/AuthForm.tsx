0a5455714834b9fcb6261e4a2e6ace5c9d478fbe
Fadhil <fadhilahdwi.a@gmail.com>
2023-11-14 19:55:04 +0700

    feat: create component button

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -96,9 +96,46 @@ const AuthForm = (props: Props) => {
             errors={errors}
           />
           <div>
-            <Button>Test</Button>
+            <Button
+              disabled={isLoading}
+              fullWidth
+              type="submit"
+            >
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
           </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute
+                inset-0
+                flex
+                items-center
+              "
+            >
+              <div
+                className="
+                  w-full
+                  border-t
+                  border-gray-300
+                "
+              >
+                <div className="relative flex justify-center text-sm">
+                  <span className="bg-white px-2 text-gray-500">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-6 flex gap-2">
+                
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,12 +1,52 @@
 'use client'
 
+import clsx from 'clsx'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  type?: 'button' | 'submit' | 'reset' | undefined
+  fullWidth?: boolean
+  children?: React.ReactNode
+  onClick?: () => void
+  secondary?: boolean
+  danger?: boolean
+  disabled?: boolean
+}
 
-const Button = (props: any) => {
+const Button: React.FC<Props> = ({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}) => {
   return (
-    <div>{props.children}</div>
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(`
+        flex
+        justify-center
+        rounded-md
+        px-3
+        py-2
+        text-sm
+        font-semibold
+        focus-visible:outline
+        focus-visible:outline-2
+        focus-visible:outline-offset-2`,
+        disabled && 'opacity-50 cursor-default',
+        fullWidth && 'w-full',
+        secondary ? 'text-gray-900' : 'text-white',
+        danger && 'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
+        !secondary && !danger && 'bg-sky-500 hover:bg-sky-500 focus-visible:outline-sky-500'
+      )}
+    >
+      {children}
+    </button>
   )
 }
 
