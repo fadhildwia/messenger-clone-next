5f4a10db449123a9b5528372843a7dea10dcd71a
Fadhil <fadhilahdwi.a@gmail.com>
2023-12-21 16:16:51 +0700

    test

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -2,13 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { getServerSession } from "next-auth"
 
 import { pusherServer } from "@/app/libs/pusher"
-// import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 
 export default async function handler(
   request: NextApiRequest, 
   response: NextApiResponse
 ) {
-  const session = await getServerSession(request, response, {})
+  const session = await getServerSession(request, response, authOptions)
 
   if (!session?.user?.email) {
     return response.status(401)
