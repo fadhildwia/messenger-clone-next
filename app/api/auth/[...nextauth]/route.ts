56d1ead447aa15aee41cd3f551fdd2268450d7f4
Fadhil <fadhilahdwi.a@gmail.com>
2023-12-21 16:12:28 +0700

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt"
-import NextAuth, { AuthOptions } from "next-auth"
+import NextAuth, { NextAuthOptions  } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
@@ -7,7 +7,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 import prisma from "@/app/libs/prismadb"
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions  = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
@@ -26,29 +26,29 @@ export const authOptions: AuthOptions = {
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error('Invalid credentials');
+          throw new Error('Invalid credentials')
         }
 
         const user = await prisma.user.findUnique({
           where: {
             email: credentials.email
           }
-        });
+        })
 
         if (!user || !user?.hashedPassword) {
-          throw new Error('Invalid credentials');
+          throw new Error('Invalid credentials')
         }
 
         const isCorrectPassword = await bcrypt.compare(
           credentials.password,
           user.hashedPassword
-        );
+        )
 
         if (!isCorrectPassword) {
-          throw new Error('Invalid credentials');
+          throw new Error('Invalid credentials')
         }
 
-        return user;
+        return user
       }
     })
   ],
@@ -59,6 +59,6 @@ export const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 }
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
